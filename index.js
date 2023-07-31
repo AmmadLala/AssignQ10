44eab78b269f519218329792b7807615a9a24ac5
Muhammad Ammad Sheikh <139677523+AmmadLala@users.noreply.github.com>
2023-07-31 14:57:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+//my name is Ammad
+// Today's date is 30th July 2023
+// below program, 1st store numbers in variable and adding them after adding storing value in variable and thn printing
+const firstNum = 25;
+const secondNum = 25;
+const msg = firstNum + secondNum;
+console.log(msg);
